205d8e52e6af4b50f98fc1d962bde5c640a832b5
Akhila <akhila.p2103@gmail.com>
2024-04-19 18:36:24 -0500

    second commit

--- cypress/e2e/Test1.cy.js
+++ cypress/e2e/Test1.cy.js
@@ -2,8 +2,9 @@ describe('first suite', () => {
 
     it('Test1', ()=>{
 
-        cy.visit('https://www.youtube.com')
+        cy.visit('https://www.google.com')
         cy.title().should('eq','YouTube')
+
     })
 }
 )
